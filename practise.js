af1da6fc05d5e21e978c2955c767170a4c5c7140
yasinDevCode <yasinit328@gmail.com>
2026-08-08 11:41:56 +0600

    Update JavaScript practice

---
+++ practise.js
@@ -0,0 +1,28 @@
+//------❤️Best--Prompt for developer❤️ 
+// সহজ ভাষায় Concept Explain (বাংলায়)
+//ES6 Format
+//Syntax
+//Step-by-Step ব্যাখ্যা
+//Memory Diagram / Flow Diagram (যেখানে প্রয়োজন)
+//Real-Life Example
+//Real Project Example
+//Practical Code Example
+//Output
+//Interview Question & Answer
+//Common Mistakes
+//Practice Tasks--🥱
+
+
+const calculateShoping=price=>{
+    if(price>1000){
+        return 0;
+    }
+    if(price>=500){
+        return 60;
+    }
+        return 100;
+    }
+
+console.log(calculateShoping(1500)); // 0
+console.log(calculateShoping(800));  // 60
+console.log(calculateShoping(300));  // 100
